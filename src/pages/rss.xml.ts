316de75bea1b566921ad6e4b8b7c66a162a1a6c5
Stefan Warringa <swarringa@intreq.nl>
2023-09-02 19:55:39 +0000

    refactor: migrate to astro 3

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,21 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+import { AppConfig } from '@/utils/AppConfig';
+
+export async function GET(context: any) {
+  return rss({
+    // `<title>` field in output xml
+    title: AppConfig.title,
+    // `<description>` field in output xml
+    description: AppConfig.description,
+    // base URL for RSS <item> links
+    // SITE will use "site" from your project's astro.config.
+    site: context.site,
+    // list of `<item>`s in output xml
+    // simple example: generate items for every md file in /src/pages
+    // see "Generating items" section for required frontmatter and advanced use cases
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    // (optional) inject custom xml
+    customData: `<language>en-us</language>`,
+  });
+}
